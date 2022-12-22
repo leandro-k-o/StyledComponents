896660013cb8f07f0be3298f4f0d4f08384ac42a
Leandro Kenji Oshiro <leandro.k.oshiro@gmail.com>
2022-12-22 08:57:25 -0300

    Header: Apenas VSCode

--- styled-component-app/src/components/Header.jsx
+++ styled-component-app/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <li>Home</li>
         <li>Sobre</li>
         <li>Contato</li>
-        <li>Item VSCode/GitHib</li>
+        <li>Item VSCode</li>
       </nav>
     </StyledHeader>
   );
